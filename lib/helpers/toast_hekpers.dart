71d61d418f9b4d177b8b9c0a7cd9bfe7a307985f
Diego Cardoso <diegocar448@hotmail.com>
2021-04-19 17:44:51 -0300

    flutterToast + helper

---
+++ lib/helpers/toast_hekpers.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class FlutterFoodToast {
+  static void error(String msg,
+      {toastLength = Toast.LENGTH_SHORT, gravity = ToastGravity.CENTER}) {
+    /* aqui vamos criar o conteúdo para o nosso metodo*/
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: toastLength,
+        gravity: gravity,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+}
