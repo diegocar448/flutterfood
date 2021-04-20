da222c5c3748e4d17302938e52b0ce9273d7578d
Diego Cardoso <diegocar448@hotmail.com>
2021-04-20 16:17:01 -0300

    Ajuste Register (Store/Screen/Exception)

--- production/flutterfood/constants/api.dart
+++
@@ -1,5 +0,0 @@
-const API_URL = 'https://1b50458ee03b.ngrok.io/api/';
-const API_VERSION = 'v1';
-const API_RECEIVE_TIMEOUT = 5000;
-const API_CONNECTION_TIMEOUT = 25000;
-//ws://127.0.0.1:53037/v-mJ0KA6G20=/ws
